2dac4ddb1c7da01b1d731ab96264a77dc7183a0a
Adrian <adrian_zinko@op.pl>
2020-05-24 09:54:49 +0200

    feat: config webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+
+    entry: './src/js/index.js',
+
+    mode: 'development',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name]-[contenthash].bundle.js',
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 3000,
+        hot: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(sass|scss)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(jpe?g|png|svg|gif)$/i,
+                use: 'file-loader',
+                options: {
+                    name: '[name][contenthash:6].ext',
+                    outputPath: 'images' 
+                }
+            }
+
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/content/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name]-[contenthash].css'
+        })
+    ]
+}
